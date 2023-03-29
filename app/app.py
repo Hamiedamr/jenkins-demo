32545ebff63a081d7a0112faee7c1a2c4b5d8c45
hamiedamr <abdelhamied.amr@outlook.com>
2023-03-30 00:20:30 +0200

    Create python app and dockerimage for it

---
+++ app/app.py
@@ -0,0 +1,11 @@
+from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
+
+app = FastAPI(title="Jenkins Demo")
+
+
+@app.get("/", response_class=HTMLResponse)
+def home():
+    return """
+    <h1> Jenkins DEMO :)!</h1>
+    """
---
+++ app/test_app.py
@@ -0,0 +1,9 @@
+from app import app
+from fastapi.testclient import TestClient
+
+client = TestClient(app)
+
+def test_read_main():
+    response = client.get("/")
+    assert response.status_code == 200
+    assert response.html() == "<h1> Jenkins DEMO :)!</h1>"
