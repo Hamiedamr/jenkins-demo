38865dfa09046f19d14081d247fb08c86ba32f72
hamiedamr <abdelhamied.amr@outlook.com>
2023-03-30 12:30:13 +0200

    fix test

--- app/test_app.py
+++ app/test_app.py
@@ -6,4 +6,4 @@ client = TestClient(app)
 def test_read_main():
     response = client.get("/")
     assert response.status_code == 200
-    assert response.text == "<h1> Jenkins DEMO :)!</h1>"
+    assert response.text == "<h1> Jenkins DEMO :)</h1>"
